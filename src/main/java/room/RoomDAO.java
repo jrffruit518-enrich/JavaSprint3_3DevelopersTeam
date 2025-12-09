22c343657f9b0d1abc34c248fdf1013d165cd8cc
jrffruit518-enrich <jrffruit518@gmail.com>
2025-12-10 00:57:30 +0100

    fix: update roomDAO

--- src/main/java/room/RoomDAO.java
+++ src/main/java/room/RoomDAO.java
@@ -28,6 +28,7 @@ private final Connection connection;
             if (affectedRows == 0) {
                 throw new RuntimeException("Database error: Room creation failed, no rows affected.");
             }
+            //El siguiente código asigna directamente el ID generado por MySQL al objeto para facilitar las consultas por ID.
             try (ResultSet generatedKeys = ps.getGeneratedKeys()) {
                 if (generatedKeys.next()) {
                     Integer newId = generatedKeys.getInt(1);
@@ -53,12 +54,37 @@ private final Connection connection;
     }
 
     @Override
-    public boolean update(Room entity) {
-        return false;
+    public boolean update(Room room) {
+        String sql = "UPDATE room SET name = ?, difficulty = ?, price = ?, theme_id = ? WHERE id_room = ?";
+        if (room.getRoomId() == null) {
+            throw new IllegalArgumentException("Cannot update Room: ID is missing.");
+        }
+        try (PreparedStatement ps = connection.prepareStatement(sql)) {
+            ps.setString(1, room.getName());
+            ps.setString(2, room.getDifficulty().toString());
+            ps.setBigDecimal(3, room.getPrice());
+            ps.setInt(4, room.getThemeId());
+            ps.setInt(5, room.getRoomId());
+            int affectedRows = ps.executeUpdate();
+            return affectedRows > 0;
+        } catch (SQLException e) {
+            e.printStackTrace();
+            throw new RuntimeException("Database error updating Room: " + room.getName(), e);
+        }
     }
 
     @Override
     public boolean delete(Id id) {
-        return false;
+        int roomId = id.value();
+        String sql = "DELETE FROM room WHERE id_room = ?";
+        try (PreparedStatement ps = connection.prepareStatement(sql)) {
+            ps.setInt(1, roomId);
+            int affectedRows = ps.executeUpdate();
+            return affectedRows > 0;
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+            throw new RuntimeException("Database error deleting Room with ID: " + roomId, e);
+        }
     }
 }
